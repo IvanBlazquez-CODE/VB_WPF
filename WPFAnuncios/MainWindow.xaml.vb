e23f0d9aadded6b20df1900f2463f8b44e837db6
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2020-02-18 18:44:42 +0100

    Add files via upload

---
+++ WPFAnuncios/MainWindow.xaml.vb
@@ -0,0 +1,17 @@
+﻿Class MainWindow
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Grid1.DataContext = New MainViewModel(modelo)
+    End Sub
+
+    Private Sub ListBox_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+        If sender.SelectedItem IsNot Nothing Then
+            Dim ventana As New Anuncios
+            ventana.MiAnunciante = sender.SelectedItem
+            ventana.Show()
+        End If
+    End Sub
+
+    Private Sub Combo1_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+
+    End Sub
+End Class
