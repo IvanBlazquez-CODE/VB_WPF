51f4349975cbdc92537efddf714b9c56388a574b
IvanBlazquez-CODE <57182443+IvanBlazquez-CODE@users.noreply.github.com>
2020-02-18 18:41:27 +0100

    Add files via upload

---
+++ WpfMateriales/MainViewModel.vb
@@ -0,0 +1,12 @@
+﻿Public Class MainViewModel
+    Dim modelo As WpfMateriales.DaniDBEntities
+    Public Sub New(model As WpfMateriales.DaniDBEntities)
+        modelo = model
+    End Sub
+
+    Public ReadOnly Property ListaMateriales As List(Of WpfMateriales.Materiales)
+        Get
+            Return modelo.Materiales.ToList
+        End Get
+    End Property
+End Class
---
+++ WpfMateriales/MainWindow.xaml.vb
@@ -0,0 +1,13 @@
+﻿Class MainWindow
+    Dim ViewModel As New MainViewModel(modelo)
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Grid1.DataContext = ViewModel
+    End Sub
+
+    Private Sub ListBox_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs)
+        Dim f As New Window1(lb.SelectedItem)
+        f.ShowDialog()
+        ViewModel = New MainViewModel(modelo)
+        Grid1.DataContext = ViewModel
+    End Sub
+End Class
